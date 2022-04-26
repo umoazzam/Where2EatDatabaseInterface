ff00e8136a9f204dd0875660a7d0ec1386c2f1b7
Usman Moazzam <usmanmoazzam@hotmail.com>
2022-04-26 13:24:15 -0400

    restructured db to fit int budgets

--- where2eatconnection.py
+++ where2eatconnection.py
@@ -14,21 +14,20 @@ try:
         record = cursor.fetchone()
         print("You're connected to database: ", record)
 
-        print("Sample Query #1: Find the top 5 restaurants that serve " + 
-                "the favorite cuisine of the user\n")
+        print("Sample Query #1: Find the top 5 restaurants that serve " +
+              "the favorite cuisine of the user\n")
 
-        query1 = ("SELECT name FROM Restaurants")
+        query1 = "SELECT name FROM restaurant, user WHERE"
+        cursor.execute(query1)
         myresult = cursor.fetchall()
         for x in myresult:
             print(x)
 
         print("Sample Query #2: Find the least visited restaurants for " +
-                "a group based on their preference\n")
+              "a group based on their preference\n")
 
         print("Sample Query #3: Find the top 5 closest restaurants that " +
-                "fit the budget for every member in the group\n")
-
-        
+              "fit the budget for every member in the group\n")
 
 except Error as e:
     print("Error while connecting to MySQL", e)
